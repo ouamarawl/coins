6b670aec67b83ea2eb5c86a003cf6b92d215fb34
ouamarawl <ouamara.wail8@gmail.com>
2024-08-18 13:52:55 +0200

    modified

---
+++ src/Component/Search.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import Axios from "axios";
+import { useEffect, useState } from "react";
+function Search() {
+    const [search, setSearch] = useState("");
+    const [crypto, setCrypto] = useState([]);
+
+    useEffect(() => {
+        Axios.get(
+            `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false`
+        ).then((res) => {
+            setCrypto(res.data);
+        });
+    }, []);
+  return (
+    <div className="App">
+    <h1>Top 10 Cryptocurrencies</h1>
+    <input
+        type="text"
+        placeholder="Search..."
+        onChange={(e) => {
+            setSearch(e.target.value);
+        }}
+    />
+    <table>
+        <thead>
+            <tr>
+                <td>Rank</td>
+                <td>Name</td>
+                <td>Symbol</td>
+                <td>Market Cap</td>
+                <td>Price</td>
+                <td>Available Supply</td>
+                <td>Volume (24hrs)</td>
+            </tr>
+        </thead>
+        <tbody>
+            {crypto
+                .filter((val) => {
+                    return val.name.toLowerCase().includes(search.toLowerCase());
+                })
+                .map((val, id) => {
+                    return (
+                        <tr key={id}>
+                            <td className="rank">{id + 1}</td>
+                            <td className="logo">
+                                <a href={`https://www.coingecko.com/en/coins/${val.id}`}>
+                                    <img src={val.image} alt="logo" width="30px" />
+                                </a>
+                                <p>{val.name}</p>
+                            </td>
+                            <td className="symbol">{val.symbol.toUpperCase()}</td>
+                            <td>${val.market_cap.toLocaleString()}</td>
+                            <td>${val.current_price.toFixed(2)}</td>
+                            <td>{val.circulating_supply.toLocaleString()}</td>
+                            <td>${val.total_volume.toLocaleString()}</td>
+                        </tr>
+                    );
+                })}
+        </tbody>
+    </table>
+</div>
+
+  )
+}
+
+export default Search
